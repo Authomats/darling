80aa8ceda5cdf5a6bd5e73e4998303ddeed88278
Authomats <thomasfelixgerdschmitt@gmail.com>
2026-09-19 17:23:38 +0200

    Output proc-macro paths depending on hicore.

--- core/src/codegen/wrap_in_const.rs
+++ core/src/codegen/wrap_in_const.rs
@@ -17,10 +17,29 @@ use syn::spanned::Spanned;
 /// - `tokens`: The trait implementations to wrap inside of `const` block
 /// - `krate`: Path to the darling crate, which defaults to `darling`
 pub fn wrap_in_const<T: ToTokens>(tokens: &T, krate: Option<&syn::Path>) -> TokenStream {
-    let use_darling = krate.map_or_else(
-        || quote! { use ::darling as _darling; },
-        |krate| quote_spanned! { krate.span() => use #krate as _darling; },
-    );
+    // Check if user depends on hicore
+    let should_fake_original = match proc_macro_crate::crate_name("hicore") {
+        Err(_) | Ok(proc_macro_crate::FoundCrate::Itself) => true,
+        Ok(proc_macro_crate::FoundCrate::Name(name)) => {
+            debug_assert_eq!(&name, "hicore");
+            false
+        }
+    };
+
+    let use_darling = if should_fake_original {
+        krate.map_or_else(
+            || quote! { use ::darling as _darling; },
+            |krate| {
+                quote_spanned! { krate.span() => use #krate as _darling; }
+            },
+        )
+    } else {
+        assert!(
+            krate.is_none(),
+            "Cannot change path of modified darling imported via hicore, import by yourself to do that!"
+        );
+        quote! { use ::hicore::darling as _darling; }
+    };
 
     quote! {
         #[doc(hidden)]
